d1e6a0d0a4ac2549649948e595292274690ba427
bharani04 <bharanibharathi0411@gmail.com>
2021-03-04 15:32:00 +0530

    changes made

--- sport/sporthome.php
+++ sport/sporthome.php
@@ -36,6 +36,8 @@
                     <li><a href="">Achievement</a></li>
                     <li><a href="">Game Know</a></li>
                     <li><a href="">Player Search</a></li>
+                    <li><a href="">Game</a></li>
+                    <li><a href="">menu</a></li>
                 </ul>
             </nav><br>
 </body>
