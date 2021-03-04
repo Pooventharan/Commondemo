2512457f26cd5b94128fc113961e0934915459dd
bharani04 <bharanibharathi0411@gmail.com>
2021-03-04 16:30:30 +0530

    occured

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 
     <style>
         fieldset{
-           margin-right: 800px;
+           margin-right: 80px;
            padding: 30px;
            background-color: #EE867C;
            width: 50%;
