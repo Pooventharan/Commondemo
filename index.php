f1136103af56ce3e902e9db80600461868fc08dd
Pooventharan <skpooventharanskp@gmail.com>
2021-03-06 11:22:51 +0530

    fieldset

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
            margin-right: 800px;
            padding: 30px;
            background-color: #EE867C;
-           width: 50%;
+           width: 70%;
            
         }
 
@@ -29,7 +29,7 @@
             
        }
        .btn{
-            padding: 9px;
+            padding: 90px;
             background-color: grey;
             border-radius: 10px;
             float: right;
